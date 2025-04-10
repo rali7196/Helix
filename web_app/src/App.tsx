ccca9088faeb4d0141de816d53a830ea21038236
Rehan <rali7196@gmail.com>
2025-04-10 13:51:10 -0700

    finished login flow

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
+import "./App.css";
+import LoginButton from "./components/LoginButton/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "./components/LogoutButton/LogoutButton";
 
 function App() {
-
-  return (
-    <p>hello</p>
-  )
+    const {user} = useAuth0();
+    return (
+        <>
+            <LoginButton />
+            <LogoutButton />
+            <p>hello</p>
+            <p>{ user ? user.email : ""}</p>
+        </>
+    );
 }
 
-export default App
+export default App;
--- web_app/src/main.tsx
+++ web_app/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Auth0Provider
+            domain="helix-dev123.us.auth0.com"
+            clientId="yFyISBN32UmnubyIFlHU1DtnXmZHmw2Y"
+            authorizationParams={{
+                redirect_uri: window.location.origin
+            }}
+        >
+            <App />
+        </Auth0Provider>
+    </StrictMode>
+);
